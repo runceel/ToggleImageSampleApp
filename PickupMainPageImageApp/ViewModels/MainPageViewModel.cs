17e68e738d4bf2c5a70f7568589d7c3a4ae113f6
Kazuki Ohta <k_ota28@hotmail.com>
2016-01-24 14:25:23 +0900

    init

---
+++ PickupMainPageImageApp/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Prism.Windows.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace PickupMainPageImageApp.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        private Visibility imageVisibility;
+
+        public Visibility ImageVisibility
+        {
+            get { return this.imageVisibility; }
+            set { this.SetProperty(ref this.imageVisibility, value); }
+        }
+
+        public void ToggleImageVisibility()
+        {
+            this.ImageVisibility = this.ImageVisibility == Visibility.Collapsed ? Visibility.Visible : Visibility.Collapsed;
+        }
+    }
+}
---
+++ PickupMainPageImageApp/Views/SampleUserControl.xaml.cs
@@ -0,0 +1,39 @@
+﻿using PickupMainPageImageApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace PickupMainPageImageApp.Views
+{
+    public sealed partial class SampleUserControl : UserControl
+    {
+        public MainPageViewModel ViewModel
+        {
+            get { return (MainPageViewModel)GetValue(ViewModelProperty); }
+            set { SetValue(ViewModelProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ViewModel.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ViewModelProperty =
+            DependencyProperty.Register("ViewModel", typeof(MainPageViewModel), typeof(SampleUserControl), new PropertyMetadata(null));
+
+
+        public SampleUserControl()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
